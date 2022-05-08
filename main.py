bb9483a5dc516ef77b6012416ebc531c66b5d444
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2022-05-08 21:30:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from urllib.request import urlopen
+import telebot
+from telebot import types
+import requests
+from bs4 import BeautifulSoup
+from telegram.ext import Updater, CommandHandler, CallbackContext, updater
+
+bot = telebot.TeleBot('5360620791:AAGQSfUeYBt56U1FhjNc6a_3iTpQdOB0HXU')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    name = types.KeyboardButton('Search the plant by its name')
+    cat = types.KeyboardButton('Search the plant by its category')
+    markup.add(name, cat)
+    mess = f'Hello, <b>{message.from_user.first_name}</b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(commands=['search'])
+def search(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("cat1", url="https://google.com"))
+    markup.add(types.InlineKeyboardButton("cat2", url="https://google.com"))
+    bot.send_message(message.chat.id, 'working...', parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=["text"])
+def check_button_option(message):
+    if message.text == 'Search the plant by its name':
+        bot.send_message(message.chat.id, 'Enter the name of the plant', parse_mode='html')
+    elif message.text == 'Search the plant by its category':
+        search(message)
+    else:
+        check_text_message(message)
+
+
+def check_text_message(message):
+    bot.send_message(message.chat.id, 'You entered ' + message.text, parse_mode='html')
+    message.text.replace(' ', '-')
+    url = "https://bloomscape.com/plant-care-guides/"
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+
+    links = []
+    tmp = url
+
+    for link in soup.findAll("a", href=True):
+        links.append(link['href'])
+        if message.text.lower() in link.get('href'):
+            tmp = link.get('href')
+            break
+
+    if tmp == url:
+        bot.send_message(message.chat.id, 'Not found!', parse_mode='html')
+    else:
+        bot.send_message(message.chat.id, tmp, parse_mode='html')
+
+
+updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + '5360620791:AAGQSfUeYBt56U1FhjNc6a_3iTpQdOB0HXU')
+
+bot.polling(none_stop=True)
